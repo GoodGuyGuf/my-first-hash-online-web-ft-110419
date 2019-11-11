0201f72b37050ddd874244aaf64f905bc4b128ee
Ethan Gustafson <ethan_gustafson@outlook.com>
2019-11-11 00:01:56 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -39,7 +39,7 @@ def adding
 
 shipping_manifest["muskets"] = 2
 shipping_manifest["gun powder"] = 4
-puts shipping_manifest
+return shipping_manifest
 
   # add 4 gun powder to the shipping_manifest hash below
 
